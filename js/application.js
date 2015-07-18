f9c3291d2612c8c56fde830ef89a46d103574a51
Tricia Ling <tricia@mydiylab.com>
2015-07-18 12:26:50 +0800

    initial commit

--- js/application.js
+++ js/application.js
@@ -1,1 +1,31 @@
+$(document).ready(function(){
+	console.log("this is working");
+	// $('#list-example li').text('fer');
+	var secondLeft = 10;
+ 	$('#timer').text(secondLeft);
 
+
+	console.log(secondLeft);
+
+	var addSecond = function(){
+		secondLeft++;
+	};
+
+	console.log(addSecond);
+
+
+	// var addSecond = function(){
+	// 	secondLeft++;
+	// };
+
+	// var minusSecond = function(){
+	// 	secondLeft--;
+	// };
+
+	// var timer = setInterval(function(){
+	// 	if (secondLeft <= 0){
+	// 		clearInterval(timer);
+	// 	}
+	// });
+
+});
